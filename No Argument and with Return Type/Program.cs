ac4922d6d7e03e0467b94cfd5f4af238fd1b69ea
Haritha R <harithalaks1998@gmail.com>
2024-07-01 16:56:17 +0530

    Add project files.

---
+++ No Argument and with Return Type/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace No_Argument_and_with_Return_Type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                // Call the GetRandomNumber function and print the result
+                int number = GetRandomNumber();
+                Console.WriteLine($"Random number: {number}");
+                Console.ReadKey();
+            }
+
+            // Function with no arguments and with return type
+            
+            int  GetRandomNumber()
+            {
+                Random random = new Random();// instance of the Random class.
+
+                return random.Next(1, 101); // Returns a random number between 1 and 100
+            }
+        }
+    }
+}
